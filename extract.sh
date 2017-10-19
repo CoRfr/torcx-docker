0a42eab452a22d1caeece8cca5c08c281dd7f60e
Bertrand Roussel <broussel@sierrawireless.com>
2017-10-19 07:37:05 -0700

    Add 17.06.2 and 17.09.0

--- extract.sh
+++ extract.sh
@@ -5,6 +5,7 @@ set -o pipefail
 
 CONTAINER_IMG=${CONTAINER_IMG:-'coreos_developer_container.bin.bz2'}
 TMP_DIR=${TMP_DIR:-$(mktemp --directory --tmpdir=$(pwd))}
+COREOS_RELEASE=${COREOS_RELEASE:-$1}
 COREOS_RELEASE=${COREOS_RELEASE:-'current'}
 CONTAINER_URL=${CONTAINER_URL:-"https://alpha.release.core-os.net/amd64-usr/${COREOS_RELEASE}/coreos_developer_container.bin.bz2"}
 MOUNTPOINT=${MOUNTPOINT:-"$TMP_DIR/rootfs"}
@@ -27,16 +28,14 @@ CONTAINER_BIN="$TMP_DIR/coreos_developer_container.bin"
 cleanup() {
     echo "Cleaning up"
 
-    if grep -q "$MOUNTPOINT" /proc/mounts; then
-        sudo umount "$MOUNTPOINT"
-        sleep 5
-    fi
+    sudo umount "$MOUNTPOINT" || true
+    sleep 5
 
     if [ -n "$CONTAINER_BIN" ]; then
         sudo kpartx -v -d "$CONTAINER_BIN"
     fi
 
-    rm -rf $TMP_DIR
+    rm -rf $TMP_DIR >/dev/null 2>&1
 }
 
 die() {
@@ -66,6 +65,15 @@ sleep 5
 
 echo "Mounting ..."
 sudo mount -o loop $ROOTFS_PART $MOUNTPOINT || die
+
+while true; do
+    if [ ! -e "${MOUNTPOINT}/usr" ]; then
+        echo "Waiting ..."
+        sleep 1
+    else
+        break
+    fi
+done
 
 echo "Copying new version ..."
 rsync -av "${MOUNTPOINT}/usr/share/torcx/store/" . || die
